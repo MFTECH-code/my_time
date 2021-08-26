88093894c547db744b2ebce2aa6606d47d0ed8e4
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-08-26 00:08:18 -0300

    Limpeza, e prototipação

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
+
+export default function App() {
+  const [data, setData] = useState(new Date().toLocaleDateString())
+  const [time, setTime] = useState(new Date().toLocaleTimeString())
+  const [marcacao, setMarcacao] = useState("inicio")
+  const [tabelaMarcacoes, setTabelaMarcacoes] = useState([{
+    tipoMarcacao: "----",
+    timeMarcaco: "--:--:--"
+  }])
+  
+  const marcacoes = []
+
+  setInterval(() => {
+    setTime(new Date().toLocaleTimeString())
+    setData(new Date().toLocaleDateString())
+  }, 1000)
+
+  const solicitarMarcacao = () => {
+    marcacoes.push({
+      tipoMarcacao: marcacao,
+      timeMarcaco: new Date().toLocaleTimeString()
+    })  
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>My Time</h1>
+        <p>Hora atual: {data}, {time}</p>
       </header>
-    </div>
-  );
+
+      <section>
+        <h2>Bem vindo!</h2>
+        <p>Solicite sua marcação!</p>
+        <select value={marcacao} onChange={(e) => setMarcacao(e.target.value)}>
+          <option value="inicio" key="inicio">Inicio</option>
+          <option value="pausa" key="pausa">Pausa</option>
+          <option value="fim" key="fim">Fim</option>
+        </select>
+        <button onClick={() => {
+          solicitarMarcacao()
+          setTabelaMarcacoes(marcacoes)
+          }}>Marcar</button>
+
+        <div>
+          <p>{tabelaMarcacoes[0].tipoMarcacao} | {tabelaMarcacoes[0].timeMarcaco}</p>
+        </div>
+      </section>
+    </>
+  )
 }
 
-export default App;
